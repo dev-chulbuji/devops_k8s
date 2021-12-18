34a4c0d80476843c007883749ab19d1e00293ce4
dj.kim <dj.kim@toss.im>
2021-12-19 00:28:45 +0900

    wip

---
+++ scripts/prometheus.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm pull --untar --untardir ../ prometheus-community/prometheus
